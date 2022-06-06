929cc601ff89399e774261035d76feec38158bca
Sangtm12 <marshll.tran.12@gmail.com>
2022-06-06 18:08:52 +0700

    fg

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelector('.btn').addEventListener('click', measureHeight);
+/* document.querySelector('.btn').addEventListener('click', measureHeight);
 function measureHeight() {
 let height = prompt('xin chào đây là chương trình đo chiều cao bằng công nghệ lượng tử cho độ chính xác đến hàng nghìn mét. Xin mời nhập chiều cao của bạn (đơn vị cm)');
-alert('Chiều cao của bạn là: ' + height + 'cm.');
-}
+alert(`Chiều cao của bạn là: ${height}cm.`);
+} */
