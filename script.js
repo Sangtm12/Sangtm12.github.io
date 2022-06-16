215dcdcd85ae7cc5f157e5360bd0eb33de3d28ce
Sangtm12 <marshll.tran.12@gmail.com>
2022-06-17 00:03:27 +0700

    theme

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const menuInput = document.querySelector('.header input')
-const menuLabel = document.querySelector('.header label')
+const btn1 = document.querySelector('.hero .btn');
+const ele = document.querySelector(':root');
+const cs = getComputedStyle(ele);
 
-menuLabel.addEventListener('click', switch);
-function switch() {
-    alert('alo');
+
+function change() {
+    if (cs.getPropertyValue('--primary') === '#e5d91c') {
+        changeToWhite();
+    } else {
+        changeToBlack();
+    }
+}
+
+function changeToWhite() {
+    ele.style.setProperty('--primary','red');
+    ele.style.setProperty('--lighter','white');
+    ele.style.setProperty('--light','lightgrey');
+    ele.style.setProperty('--dark','grey');
+    ele.style.setProperty('color','black');
+    ele.style.setProperty('--metaText','black');
+}
+
+function changeToBlack() {
+    ele.style.setProperty('--primary','#e5d91c');
+    ele.style.setProperty('--lighter','#333438');
+    ele.style.setProperty('--light','#2d2e32');
+    ele.style.setProperty('--dark','#25262a');
+    ele.style.setProperty('--metaText','lightgrey');
+    ele.style.setProperty('color','white');
 }
