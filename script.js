78efb1e9ded3744882997fcc33a62573a37e855c
Sangtm12 <marshll.tran.12@gmail.com>
2022-07-06 14:59:59 +0700

    nop cv

--- script.js
+++ script.js
@@ -20,22 +20,3 @@ function change() {
 
 const themeBtn = document.getElementById('theme');
 themeBtn.addEventListener('click', change);
-
-/* function changeToWhite() {
-    ele.style.setProperty('--primary','red');
-    ele.style.setProperty('--lighter','white');
-    ele.style.setProperty('--light','lightgrey');
-    ele.style.setProperty('--dark','grey');
-    ele.style.setProperty('color','black');
-    ele.style.setProperty('--metaText','black');
-}
-
-function changeToBlack() {
-    ele.style.setProperty('--primary','#e5d91c');
-    ele.style.setProperty('--lighter','#333438');
-    ele.style.setProperty('--light','#2d2e32');
-    ele.style.setProperty('--dark','#25262a');
-    ele.style.setProperty('--metaText','lightgrey');
-    ele.style.setProperty('color','white');
-}
- */
