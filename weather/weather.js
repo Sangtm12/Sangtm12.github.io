daa55033bc4955708c6d945bc4a7ac1af0b1a0c8
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2023-06-07 00:07:45 +0800

    progress

--- weather/weather.js
+++ weather/weather.js
@@ -9,12 +9,16 @@ document.querySelector('.search__form').addEventListener('submit', (e)=>{
         input.setAttribute('placeholder','Input cannot be blank!');
         input.focus();
     } else {
+        sessionStorage.setItem('currentCity',input.value);
         input.setAttribute('placeholder','Search city...');
         fetchToday(input.value);
-        fetchHourly(input.value);
+        fetchHourly(input.value); //will call fetchDaily later
         input.value = '';
     }
 });
+
+fetchToday(sessionStorage.getItem('currentCity') ? sessionStorage.getItem('currentCity') :'kuala lumpur');
+fetchHourly(sessionStorage.getItem('currentCity') ? sessionStorage.getItem('currentCity') :'kuala lumpur');
 
 // Today, hourly and tomorrow are 3 different API requests.
 // Because of,... budget problems.
@@ -57,7 +61,13 @@ function fetchHourly(city) {
 }
 
 function displayHourly(data) {
+    //first, get the latitude and longitude from the data to send to another API;
+    //because this API doesn't give daily info, and the other one requires latitude, longitude and timezone;
     
+    let {lat, lon} = data.city.coord;
+    fetchDaily(lat,lon);
+
+
     //get 6 timestamps
     let timeCtn = document.querySelector('.hourly__time');
     timeCtn.innerHTML = '';
@@ -93,17 +103,61 @@ function displayHourly(data) {
 
         if (i === 4) {
             chartCtn.innerHTML += `<div class="chart--item">
-            <div class="chart--temp" style="top: ${(50 * (1-ratio)) - 15}%">${Math.round(dataArray[i].temp)}&deg;C</div>
-            <div class="chart--temp last"style="top: ${(50 * (1-nextRatio)) - 15}%">${Math.round(dataArray[i+1].temp)}&deg;C</div>
-            <div class="chart--line" style="clip-path: polygon(0 ${50 * (1-ratio)}%, 100% ${50 * (1-nextRatio)}%, 100% 100%, 0 100%)"></div>
-        </div>`;
+                                <div class="chart--temp" style="top: ${50 * (1 - ratio) - 15
+                                }%">${Math.round(dataArray[i].temp)}&deg;C</div>
+                                <div class="chart--temp last"style="top: ${
+                                  50 * (1 - nextRatio) - 15
+                                }%">${Math.round(dataArray[i + 1].temp)}&deg;C</div>
+                                <div class="chart--line" style="clip-path: polygon(0 ${50 * (1 - ratio)}%, 100% ${50 * (1 - nextRatio)}%, 100% 100%, 0 100%)"></div></div>`;
         } else {
             chartCtn.innerHTML += `<div class="chart--item">
                                     <div class="chart--temp" style="top: ${(50 * (1-ratio)) - 15}%">${Math.round(dataArray[i].temp)}&deg;C</div>
                                     <div class="chart--line" style="clip-path: polygon(0 ${50 * (1-ratio)}%, 100% ${50 * (1-nextRatio)}%, 100% 100%, 0 100%)"></div>
                                 </div>`;
-        }
+        }                     
+    }
+}
 
-                         
+function fetchDaily(lat,lon) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {displayDaily(data)})
+        .catch((error) => {console.log(error)})
+}
+
+let globalData;
+let dayArray = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+function wmoCodeToIconClass(code) {
+    if (code == 0) {
+        return 'fa-solid fa-sun';
+    } else if (code >= 51 && code <= 67 || code >= 80 && code <= 99) {
+        return 'fa-solid fa-cloud-rain';
+    } else {
+        return 'fa-solid fa-cloud';
+    }
+}
+
+
+function displayDaily(data) {
+    globalData = data;
+    let dailyCtn = document.querySelector('.daily__day__ctn');
+    dailyCtn.innerHTML = '';
+
+    //looping 5 times to print 5 days
+    for (let i = 0; i <= 4; i++) {
+
+    let day = dayArray[new Date(data.daily.time[i]).getDay()].toUpperCase();
+
+        dailyCtn.innerHTML += `<div class="daily__day">
+        <div class="daily__day__title">${i == 0?'TODAY':day}</div>
+        <div class="daily__day__icon">
+            <i class="${wmoCodeToIconClass(data.daily.weathercode[i])}"></i>
+        </div>
+        <div class="daily__day__lohi">
+            <span class="deg-low">${Math.round(data.daily.temperature_2m_min[i])}/</span>
+            <span class="deg-high">${Math.round(data.daily.temperature_2m_max[i])}°C</span>
+        </div>
+    </div>`;
     }
 }
