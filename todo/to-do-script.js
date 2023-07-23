443917933e2f34c76db4e1c50df7c5d355fd222b
Sangtm12 <105406591+Sangtm12@users.noreply.github.com>
2023-07-23 20:58:50 +0800

    fix bugs

--- todo/to-do-script.js
+++ todo/to-do-script.js
@@ -1,45 +1,46 @@
-const input = document.querySelector('#msg');
-const addButton = document.querySelector('.add-new button');
-const form = document.querySelector('form.add-new');
-let taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
-const taskCtn = document.querySelector('.task-ctn');
+const input = document.querySelector("#msg");
+const addButton = document.querySelector(".add-new button");
+const form = document.querySelector("form.add-new");
+let taskArray = JSON.parse(localStorage.getItem("taskArray")) || [];
+const taskCtn = document.querySelector(".task-ctn");
 
-document.querySelector('.clear').addEventListener('click', () => {
-    window.localStorage.removeItem('taskArray');
-    taskArray = [];
-    draw();
-})
+document.querySelector(".clear").addEventListener("click", () => {
+  window.localStorage.removeItem("taskArray");
+  taskArray = [];
+  draw();
+});
 
 draw();
 
 // ADD NEW TASK
 
-form.addEventListener('submit',verifyMsg);
+form.addEventListener("submit", verifyMsg);
 
 function verifyMsg(e) {
-    e.preventDefault();
-    console.log('vrf');
-    const msg = input.value;
-    input.value = '';
-    if (msg == '') {
-        input.setAttribute('placeholder','Text must not be empty!')
-    } else {
-        addTask(msg);
-    }
+  e.preventDefault();
+  console.log("vrf");
+  const msg = input.value;
+  input.value = "";
+  if (msg == "") {
+    input.setAttribute("placeholder", "Text must not be empty!");
+  } else {
+    addTask(msg);
+  }
 }
 
 function addTask(msg) {
-    input.value = '';
-    input.focus();
-    input.setAttribute('placeholder','Add a new task...');
-    taskArray.push(msg);
-    draw();
+  input.value = "";
+  input.focus();
+  input.setAttribute("placeholder", "Add a new task...");
+  taskArray.shift(msg);
+  draw();
 }
 
 function draw() {
-    taskCtn.innerHTML = '';
-    taskArray.forEach((item) => {
-        taskCtn.innerHTML = `
+  taskCtn.innerHTML = "";
+  taskArray.forEach((item) => {
+    taskCtn.innerHTML =
+      `
         <div class="task ongoing">
                     <div class="checkbox-ctn">
                         <input type="checkbox" name="checkbox" class="checkbox">
@@ -60,117 +61,117 @@ function draw() {
                     </div>
                 </div>
         ` + taskCtn.innerHTML;
+  });
+
+  function assignButton(selector, callback) {
+    document.querySelectorAll(selector).forEach((item) => {
+      item.addEventListener("click", callback);
     });
+  }
 
-    function assignButton(selector, callback) {
-        document.querySelectorAll(selector).forEach(item => {item.addEventListener('click', callback);})
-    }
-
-    assignButton('.checkbox', changeStatus);
-    assignButton('i.fa-pen-to-square', editTask);
-    assignButton('i.fa-trash', removeTask);
-    assignButton('i.fa-caret-up', moveUp);
-    assignButton('i.fa-caret-down', moveDown);
-    if (taskArray.length !== 0) {
-        window.localStorage.setItem('taskArray', JSON.stringify(taskArray));
-    }
+  assignButton(".checkbox", changeStatus);
+  assignButton("i.fa-pen-to-square", editTask);
+  assignButton("i.fa-trash", removeTask);
+  assignButton("i.fa-caret-up", moveUp);
+  assignButton("i.fa-caret-down", moveDown);
+  if (taskArray.length !== 0) {
+    window.localStorage.setItem("taskArray", JSON.stringify(taskArray));
+  }
 }
 
 //CHANGE STATUS - MARK AS DONE
 
 function changeStatus() {
-    if (this.parentNode.parentNode.classList.contains('ongoing')) {
-        this.parentNode.parentNode.classList.remove('ongoing');
-        this.parentNode.parentNode.classList.add('done');
-        let movedTask = this.parentNode.parentNode;
-        movedTask.parentNode.removeChild(movedTask);
-        taskCtn.append(movedTask)
-    } else {
-        this.parentNode.parentNode.classList.remove('done');
-        this.parentNode.parentNode.classList.add('ongoing');
-        let movedTask = this.parentNode.parentNode;
-        movedTask.parentNode.removeChild(movedTask);
-        taskCtn.prepend(movedTask)
-    }
+  if (this.parentNode.parentNode.classList.contains("ongoing")) {
+    this.parentNode.parentNode.classList.remove("ongoing");
+    this.parentNode.parentNode.classList.add("done");
+    let movedTask = this.parentNode.parentNode;
+    movedTask.parentNode.removeChild(movedTask);
+    taskCtn.append(movedTask);
+  } else {
+    this.parentNode.parentNode.classList.remove("done");
+    this.parentNode.parentNode.classList.add("ongoing");
+    let movedTask = this.parentNode.parentNode;
+    movedTask.parentNode.removeChild(movedTask);
+    taskCtn.prepend(movedTask);
+  }
 }
 
 //EDIT TASK
 
 function editTask(event) {
-    let task = event.target.parentNode.parentNode;
-    task.classList.add('focus');
-    input.focus();
-    input.value = task.querySelector('.task-title').innerText;
-    form.removeEventListener('submit',verifyMsg);
-    form.addEventListener('submit',updateTask)
-    addButton.innerText = 'Update';
+  let task = event.target.parentNode.parentNode;
+  task.classList.add("focus");
+  input.focus();
+  input.value = task.querySelector(".task-title").innerText;
+  form.removeEventListener("submit", verifyMsg);
+  form.addEventListener("submit", updateTask);
+  addButton.innerText = "Update";
 }
 
 function updateTask(e) {
-    e.preventDefault();
-    let task = document.querySelector('.task.focus')
-    let msg = input.value;
-    if (msg == '') {
-        input.setAttribute('placeholder','Text must not be empty!')
-    } else {
-        document.querySelectorAll('.task').forEach((item, index)=>{
-            if (item == task) {
-                taskArray.splice(taskArray.length - 1 - index, 1, msg);
-            }
-        })
-        draw();
-        task.classList.remove('focus');
-        input.focus();
-        form.removeEventListener('submit', updateTask);
-        form.addEventListener('submit', verifyMsg);
-        addButton.innerText = 'Add task';
-        document.querySelector('#msg').value = '';
-    }
-} 
+  e.preventDefault();
+  let task = document.querySelector(".task.focus");
+  let msg = input.value;
+  if (msg == "") {
+    input.setAttribute("placeholder", "Text must not be empty!");
+  } else {
+    document.querySelectorAll(".task").forEach((item, index) => {
+      if (item == task) {
+        taskArray.splice(taskArray.length - 1 - index, 1, msg);
+      }
+    });
+    draw();
+    task.classList.remove("focus");
+    input.focus();
+    form.removeEventListener("submit", updateTask);
+    form.addEventListener("submit", verifyMsg);
+    addButton.innerText = "Add task";
+    document.querySelector("#msg").value = "";
+  }
+}
 
 //REMOVE TASK
 
 function removeTask(event) {
-    let task = event.target.parentNode.parentNode;
-    for (let i = 0; i < taskArray.length; i++) {
-        if (task.querySelector('.task-title').innerText == taskArray[i]) {
-            taskArray.splice(i,1)
-        }
+  let task = event.target.parentNode.parentNode;
+  for (let i = 0; i < taskArray.length; i++) {
+    if (task.querySelector(".task-title").innerText == taskArray[i]) {
+      taskArray.splice(i, 1);
     }
-    draw();
-    input.focus();
+  }
+  draw();
+  input.focus();
 }
 
 //move task
 
 function moveUp(event) {
-    let task = event.target.parentNode.parentNode;
-    for (let i = 0; i < taskArray.length; i++) {
-        if (task.querySelector('.task-title').innerText == taskArray[i]) {
-            if (i == taskArray.length - 1) {
-                return;
-            } else {
-                console.log(i);
-                taskArray.splice(i+1, 0, taskArray.splice(i, 1));
-                draw();
-                break;
-            }
-        }
+  let task = event.target.parentNode.parentNode;
+  for (let i = 0; i < taskArray.length; i++) {
+    if (task.querySelector(".task-title").innerText == taskArray[i]) {
+      if (i == taskArray.length - 1) {
+        return;
+      } else {
+        console.log(i);
+        taskArray.splice(i + 1, 0, taskArray.splice(i, 1));
+        draw();
+        break;
+      }
     }
+  }
 }
 
 function moveDown(event) {
-    let task = event.target.parentNode.parentNode;
-    for (let i = 0; i < taskArray.length; i++) {
-        if (task.querySelector('.task-title').innerText == taskArray[i]) {
-            if (i == 0) {
-                return;
-            } else {
-                taskArray.splice(i-1, 0, taskArray.splice(i, 1));
-                draw();
-            }
-        }
-    };
+  let task = event.target.parentNode.parentNode;
+  for (let i = 0; i < taskArray.length; i++) {
+    if (task.querySelector(".task-title").innerText == taskArray[i]) {
+      if (i == 0) {
+        return;
+      } else {
+        taskArray.splice(i - 1, 0, taskArray.splice(i, 1));
+        draw();
+      }
+    }
+  }
 }
-
-
